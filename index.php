6f17b594bbe5c237c03baea3202cff4b3eb7a27a
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-05 21:01:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <frameset rows= "30%, 40%, 30%">
 <Frame src= "red.html">
 <<Frame src= "Green.html">
-<Frame src= "Blue.html">
+<Frame src= "file:///C:/Users/kyana123/Desktop/SAMPLE%20(1)/Blue.html">
 </frameset>
 
 </html>
